c84cd33fade36bff187b0014bc74adeb79feec65
taka16-tnk <taka16.tanaka@gmail.com>
2025-05-20 23:45:17 +0900

    Add post form and stylesheet

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.get("/", (req, res) => {
   res.render("index", { posts });
 });
 
+// 投稿フォーム
+app.post("/post", (req, res) => {
+  const { title, content } = req.body;
+
+  if (title && content) {
+    posts.push({
+      id: Date.now().toString(),
+      title,
+      content
+    });
+  }
+
+  res.redirect("/");
+});
+
 // サーバー起動
 app.listen(3000, () => {
   console.log("http://localhost:3000 で起動中");
